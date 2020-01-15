27b8704c886503ffb49f5114fe5d8bf6d910f4ec
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2020-01-15 17:33:15 +0600

    #01 - functional style rewrite

--- 2019/01/solver.php
+++ 2019/01/solver.php
@@ -1,10 +1,5 @@
 <?php declare(strict_types=1);
-
-// reading test data
-$modules = file('./modules.txt', FILE_IGNORE_NEW_LINES);
-
-// typecasting to int
-$modules = array_map('\intval', $modules);
+$modules = array_map('\intval', file('./modules.txt', FILE_IGNORE_NEW_LINES));
 
 /**
  * Takes mass and returns required fuel for that
@@ -13,30 +8,30 @@ $modules = array_map('\intval', $modules);
  *
  * @return int
  */
-function fuel_requirement(int $mass): int
-{
+$fuel_requirement_func = static function (int $mass): int {
     return abs((int)floor($mass / 3) - 2);
-}
+};
 
-function total_fuel_requirement(int $mass): int
-{
+$total_fuel_requirement = static function (int $mass) use ($fuel_requirement_func): int {
     $fuel_requirement = 0;
     $minimal_free_weight = 8; // (|floor(8/3)| - 2) = 0
     $requirement = $mass;
     while ($requirement > $minimal_free_weight) {
-        $requirement = fuel_requirement($requirement);
+        $requirement = $fuel_requirement_func($requirement);
         $fuel_requirement += $requirement;
     }
     return $fuel_requirement;
-}
+};
 
-// some functional style php
-$modules_fuel_required = array_reduce(
-    $modules,
-    static function (int $carry, int $module_weight): int {
-        return $carry + total_fuel_requirement($module_weight);
-    },
-    0
-);
+$requirement_calculator = static function ($modules, callable $calculator_func) {
+    return array_reduce(
+        $modules,
+        static function (int $carry, int $module_weight) use ($calculator_func): int {
+            return $carry + $calculator_func($module_weight);
+        },
+        0
+    );
+};
 
-printf("Fuel requirement: $modules_fuel_required\n");
+printf("Only modules fuel requirement: %d\n", $requirement_calculator($modules, $fuel_requirement_func));
+printf("Fuel total requirement: %d\n", $requirement_calculator($modules, $total_fuel_requirement));
