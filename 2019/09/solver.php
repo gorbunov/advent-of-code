0b98c81bad71210161c8f94fda1cab86c21e5669
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2020-01-23 20:57:30 +0600

    Fixed reboot of IntCodeRunner

--- 2019/09/solver.php
+++ 2019/09/solver.php
@@ -3,8 +3,11 @@ require_once __DIR__.'/../shared/autoload.php';
 
 $program = file_get_contents('./program.txt', false);
 
-$cpu = \IntCode\IntCodeComputer::load($program, \IntCode\Program\InputFactory::create([1]));
-$cpu = $cpu->run();
+$cpu = \IntCode\IntCodeComputer::load($program);
+$cpu = $cpu->run(function (\IntCode\Program $program) {
+    $program->input()->insert(1);
+    return $program;
+});
 
 print_array_values($cpu->output()->outputs());
 printf("BOOST keycode: %d\n", $cpu->output()->outputs()[0]);
