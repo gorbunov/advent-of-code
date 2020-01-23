24abe90292cd2c8fe048e1548599efb4a687db08
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2020-01-23 21:00:26 +0600

    Solved puzzle 09, both parts!

--- 2019/09/solver.php
+++ 2019/09/solver.php
@@ -4,10 +4,16 @@ require_once __DIR__.'/../shared/autoload.php';
 $program = file_get_contents('./program.txt', false);
 
 $cpu = \IntCode\IntCodeComputer::load($program);
-$cpu = $cpu->run(function (\IntCode\Program $program) {
+$program = $cpu->run(function (\IntCode\Program $program) {
     $program->input()->insert(1);
     return $program;
 });
 
-print_array_values($cpu->output()->outputs());
-printf("BOOST keycode: %d\n", $cpu->output()->outputs()[0]);
+printf("BOOST keycode: %d\n", $program->output()->outputs()[0]);
+
+$program = $cpu->run(function (\IntCode\Program $program) {
+    $program->input()->insert(2);
+    return $program;
+});
+
+printf("Distress code: %d\n", $program->output()->outputs()[0]);
