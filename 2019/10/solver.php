eb9ad9c93d22472549b13a737f57d6e93204f428
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2020-01-24 11:16:49 +0600

    Solved puzzle #10_01

--- 2019/10/solver.php
+++ 2019/10/solver.php
@@ -4,7 +4,4 @@ $map = file('./asteroids.txt', FILE_IGNORE_NEW_LINES);
 
 $map = \Asteroids\Map::load($map);
 
-print_array_values($map->anglesToAsteroidsFrom(\Asteroids\Position::create(10, 10)));
-
-//var_dump(\Asteroids\Position::create(3, 4)->angle(\Asteroids\Position::create(1, 0)));
-//var_dump(\Asteroids\Position::create(3, 4)->angle(\Asteroids\Position::create(2, 2)));
+printf("Best location is %s, with %d detections\n", ...$map->bestStationLocation());
