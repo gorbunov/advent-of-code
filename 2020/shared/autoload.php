9a1603e33091478b3edbac0427e548eb4a6d23f8
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2020-11-20 14:03:30 +0600

    GET READDYYYYYY

---
+++ 2020/shared/autoload.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+require_once __DIR__ . '/functions.php';
+spl_autoload_register(
+    static function ($classname) {
+        /** @noinspection PhpIncludeInspection */
+        require_once sprintf('%s/%s.php', __DIR__, str_replace('\\', DIRECTORY_SEPARATOR, $classname));
+    }
+);
