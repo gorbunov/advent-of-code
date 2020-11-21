b5a6cfb04a3694ee0563592ad8fcb63620328317
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2020-11-21 16:43:00 +0600

    Solved 2015/12

--- 2015/12/solver.php
+++ 2015/12/solver.php
@@ -3,16 +3,22 @@ require_once __DIR__.'/../shared/autoload.php';
 
 $json = json_decode(file_get_contents('./accounting.json'), true, 512, JSON_THROW_ON_ERROR);
 
-$sum = 0;
+function section_amount(array $array): int
+{
+    $total = 0;
+    if (in_array("red", $array, true) && count(array_filter(array_keys($array), '\is_string')) > 0) {
+        return 0;
+    }
 
-array_walk_recursive(
-    $json,
-    static function ($value, $key) use (&$sum) {
+    foreach ($array as $key => $value) {
         if (is_int($value)) {
-            $sum += $value;
+            $total += $value;
         }
-    },
-    $sum
-);
+        if (is_array($value)) {
+            $total += section_amount($value);
+        }
+    }
+    return $total;
+}
 
-print $sum;
+print section_amount($json);
